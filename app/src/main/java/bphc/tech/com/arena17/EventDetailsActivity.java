84dbe4e9a178f20d8f394ff88e3b065078d20331
sarathisalwaysbusy <sarathisalwaysbusy@gmail.com>
2016-12-26 17:39:22 +0530

    minor boiler code Picasso

--- app/src/main/java/bphc/tech/com/arena17/EventDetailsActivity.java
+++ app/src/main/java/bphc/tech/com/arena17/EventDetailsActivity.java
@@ -13,7 +13,11 @@ import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -27,49 +31,38 @@ import bphc.tech.com.arena17.sets.EventsSet;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
-
     SectionsPagerAdapter mSectionsPagerAdapter;
-
     ViewPager mViewPager;
     Toolbar toolbar;
     CollapsingToolbarLayout collapsingToolbar;
     NestedScrollView nestedScrollView;
     EventsSet event;
+    KenBurnsView eventImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
+        //Instantiate all views
+        eventImage = (KenBurnsView) findViewById(R.id.event_image);
+        mViewPager = (ViewPager) findViewById(R.id.container);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        // Setup Tab Layout
         tabLayout.addTab(tabLayout.newTab().setText("Details"));
         tabLayout.addTab(tabLayout.newTab().setText("Schedule"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+        Picasso.with(this)
+                .load("https://www.simplifiedcoding.net/wp-content/uploads/2015/10/advertise.png")
+                .into(eventImage);
+
+        //MyAdapter
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
         tabLayout.setupWithViewPager(mViewPager);
-
-        /*      setUpWithViewPager will take care of the below code: :-)
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                mViewPager.setCurrentItem(tab.getPosition());
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });*/
-
 
         toolbar = (Toolbar) findViewById(R.id.events_toolbar);
         setSupportActionBar(toolbar);
@@ -83,48 +76,61 @@ public class EventDetailsActivity extends AppCompatActivity {
         }
 
         //Obtain the Event.
-        try{
-            DBHelper helper = new DBHelper(this);
-            int eventID = getIntent().getIntExtra(Constants.Arg_Event_ID,-1);
-            if (eventID != -1){
-                List<EventsSet> eventList = helper.getEventData(eventID);
-                event = eventList.get(0);
-                fillDetails(event);
-            }
-            else {
-                Toast.makeText(this,"Sorry.. no such event found..!!",Toast.LENGTH_SHORT).show();}
-        }catch (Exception e){
-            Toast.makeText(this,"Error getting event",Toast.LENGTH_SHORT).show();
-            collapsingToolbar.setTitle("Inside catch");
-        }
+        getAndFillEventData();
 
-        nestedScrollView = (NestedScrollView) findViewById (R.id.NestedScrollView);
-        nestedScrollView.setFillViewport (true); // needed for the nestedScrollView to be displayed.
+        // needed for the nestedScrollView to be displayed.
+        nestedScrollView = (NestedScrollView) findViewById(R.id.NestedScrollView);
+        nestedScrollView.setFillViewport(true);
     }
 
-    private void fillDetails(EventsSet event){
-        if(event.getName().isEmpty()){
+    private void fillData(EventsSet event) {
+        if (event.getName().isEmpty()) {
             collapsingToolbar.setTitle("Error");
-        }
-        else{
+        } else {
             collapsingToolbar.setTitle(event.getName());
+            Picasso.with(this)
+                    .load("YOUR IMAGE URL HERE")
+                    //.placeholder(DRAWABLE RESOURCE)   // optional
+                    //.error(DRAWABLE RESOURCE)      // optional
+                    .into(eventImage);
+        }
+    }
+
+    private void getAndFillEventData() {
+        try {
+            DBHelper helper = new DBHelper(this);
+            int eventID = getIntent().getIntExtra(Constants.Arg_Event_ID, -1);
+            if (eventID != -1) {
+                List<EventsSet> eventList = helper.getEventData(eventID);
+                event = eventList.get(0);
+                fillData(event);        // Filling Data
+            } else {
+                Toast.makeText(this, "Sorry.. no such event found..!!", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Error getting event", Toast.LENGTH_SHORT).show();
+            collapsingToolbar.setTitle("Inside catch");
         }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_star,menu);
+        getMenuInflater().inflate(R.menu.toolbar_star, menu);
         MenuItem item = menu.findItem(R.id.menu_star);
-        // TODO: 25-12-2016 add appropriate ic for star
-        //if isFavorite(eventID){setIcon selected} **
-        //item.setIcon(R.drawable.ic_star_selected);
+        if (item.getIcon() == null) {
+            item.setVisible(false);
+        } else {
+            // TODO: 25-12-2016 add appropriate ic for star
+            //if isFavorite(eventID){setIcon selected} **
+            //item.setIcon(R.drawable.ic_star_selected);
+        }
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         return super.onOptionsItemSelected(item);
     }
 
